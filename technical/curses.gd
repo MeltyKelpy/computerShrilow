80f7654c8a507544351a23be7ad7c66000f80926
MeltyKelpy <91714683+MeltyKelpy@users.noreply.github.com>
2025-03-13 12:15:10 -0600

    FUCL

---
+++ technical/curses.gd
@@ -0,0 +1,11 @@
+extends Node
+
+var mainCurses = [
+	{"Name":"Curse of Gambling",
+	"Icon":"res://assets/images/ui/curses/gambling.png",}
+	]
+var passiveCurses = []
+
+var curses = []
+
+var spiceLevel = 1
