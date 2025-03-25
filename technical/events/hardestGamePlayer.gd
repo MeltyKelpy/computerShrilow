8ce19dff95d5d4c9a3e9ba3d3007c8d5d2d4c70f
MeltyKelpy <mehbeingrandom@gmail.com>
2025-03-25 16:56:01 -0600

    fast

--- technical/events/hardestGamePlayer.gd
+++ technical/events/hardestGamePlayer.gd
@@ -13,13 +13,13 @@ func _physics_process(delta: float) -> void:
 	
 	if can == true:
 		if Input.is_action_pressed("ui_up"):
-			position.y -= 1.5
+			position.y -= 3
 		if Input.is_action_pressed("ui_down"):
-			position.y += 1.5
+			position.y += 3
 		if Input.is_action_pressed("ui_left"):
-			position.x -= 1.5
+			position.x -= 3
 		if Input.is_action_pressed("ui_right"):
-			position.x += 1.5
+			position.x += 3
 		
 	move_and_slide()
 
